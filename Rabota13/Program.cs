485db968b7781b92310e063a476dde6c75123267
Vladislav <vladperov@mail.ru>
2022-11-11 01:44:11 +0500

    13

---
+++ Rabota13/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int N =23;
+int[] arr = new int[];
+int[] Sic(N)
+Print (arr);
+{
+    for(int j = 0; j < array.Length; j++) arr[i]=Sic(N-1);
+    return arr;
+}
+
+void Print (int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+      Console.Write(array[j] + " ");
+    }
+}
